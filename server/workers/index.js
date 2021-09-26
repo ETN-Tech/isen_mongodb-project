a8bde8fc94fa67d4dd55c2f08b013f8311eaea7e
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-09-26 23:38:34 +0200

    Reorganized workers/index, renamed routes

--- server/workers/index.js
+++ server/workers/index.js
@@ -5,10 +5,12 @@ module.exports = router
 const axios = require('axios');
 const {response} = require("express");
 
+
 const axiosLille = axios.get('https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=10');//api lille
 const axiosParis = axios.get('https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes');//api paris
 //const axiosLyon = axios.get('https://download.data.grandlyon.com/ws/rdata/sit_sitra.sittourisme/all.json?maxfeatures=100&start=1');//api lYON
 const axiosRennes = axios.get('https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=etat-des-stations-le-velo-star-en-temps-reel&q=&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles');//api rennes
+
 
 // MongoDB database connection
 const { MongoClient, ObjectId } = require("mongodb");
@@ -25,158 +27,36 @@ MongoClient.connect(process.env.DATABASE_URI, function(err, client) {
 });
 
 
-router.get('/ville', (req, res) => {
+/* ---- API ROUTING ----- */
 
-    // TODO request data from VLille API
-    GetDataVille();
-    SetDataVille();
+router.get('/static', (req, res) => {
+
+    // Clear collection
+    stations_static.remove({});
+
+    // on va charger les informations de nous 4 villes ici;
+    getDLille();
+    getDParis();
+    //getDLyon();
+    getDRennes()
+
+    res.status(200).json();
+})
+
+router.get('/dynamic', (req, res) => {
+
+    // Clear collection
+    stations_dynamic.remove({});
+
+    setDLille();
+    setDParis();
+    setDRennes();
 
     res.status(200).json();
 })
 
 
-function SetDataVille(){
-    setDLille();
-    setDParis();
-    setDRennes();
-}
-
-function setDLille(){
-    axiosLille.then(reponse => {
-            reponse.data.records.forEach(function (element){
-                var size = element.fields.nbvelosdispo + element.fields.nbplacesdispo;
-                //on charge la collection stations_dynamic_history
-                stations_static
-                    .updateOne(
-                        {"stationId": element.recordid}, // Filter
-                        {$set: {
-
-                                "city": element.fields.commune.toUpperCase(),
-                                "name": element.fields.nom.toUpperCase(),
-                                "geolocation": element.fields.localisation,
-                                "size": size,
-                                "tpe": element.fields.type.includes("AVEC TPE"),
-                                "available": null,
-                                "updatedAt": element.fields.datemiseajour
-                            }} // Update
-                    )
-                    .then((obj) => {
-                        console.log('Updated - ' + obj);
-                        response.redirect('orders')
-                    })
-                    .catch((err) => {
-                        console.log('Error: ' + err);
-                    })
-                    stations_dynamic.insertOne({
-                    "stationId": element.recordid,
-                    "bikesAvailable": element.fields.nbvelosdispo,
-                    "docksAvailable": element.fields.nbplacesdispo,
-                    "createdAt": element.fields.datemiseajour
-                })
-            })
-
-                //on charge la collection stations_dynamic
-
-
-            })
-
-        .catch(error => {
-            console.log(error);
-        });
-}
-
-function setDParis(){
-    axiosParis.then(reponse => {
-        reponse.data.records.forEach(function (element){
-            //on charge la collection stations_dynamic_history
-            stations_static
-                .updateOne(
-                    {"stationId": element.recordid}, // Filter
-                    {$set: {
-                            "city": element.fields.nom_arrondissement_communes.toUpperCase(),
-                            "name": element.fields.name.toUpperCase(),
-                            "geolocation": element.fields.coordonnees_geo,
-                            "size": element.fields.capacity,
-                            "tpe": element.fields.is_renting.includes("OUI"),
-                            "available": element.fields.is_installed.includes("OUI"),
-                            "updatedAt": element.record_timestamp
-                        }} // Update
-                )
-                .then((obj) => {
-                    console.log('Updated - ' + obj);
-                    response.redirect('orders')
-                })
-                .catch((err) => {
-                    console.log('Error: ' + err);
-                })
-
-
-            //on charge la collection stations_dynamic
-            var bikesAvailable = element.fields.capacity - element.fields.numdocksavailable;
-            stations_dynamic.insertOne({
-                "stationId": element.recordid,
-                "bikesAvailable": bikesAvailable,
-                "docksAvailable": element.fields.numdocksavailable,
-                "createdAt": element.record_timestamp
-            })
-
-        })
-    })
-        .catch(error => {
-            console.log(error);
-        });
-}
-
-function setDRennes(){
-    var city = "RENNES";
-    var tpe = false;
-    axiosRennes.then(reponse => {
-        reponse.data.records.forEach(function (element){
-            //on charge la collection stations_dynamic_history
-            stations_static
-                .updateOne(
-                    {"stationId": element.recordid}, // Filter
-                    {$set: {
-                            "city": city.toUpperCase(),
-                            "name": element.fields.nom.toUpperCase(),
-                            "geolocation": element.fields.coordonnees,
-                            "size": element.fields.nombreemplacementsactuels,
-                            "tpe": tpe,
-                            "available": null,
-                            "updatedAt": element.fields.lastupdate
-                        }} // Update
-                )
-                .then((obj) => {
-                    console.log('Updated - ' + obj);
-                    response.redirect('orders')
-                })
-                .catch((err) => {
-                    console.log('Error: ' + err);
-                })
-
-            //on charge la collection stations_dynamic
-            stations_dynamic.insertOne({
-                "stationId": element.recordid,
-                "bikesAvailable": element.fields.nombrevelosdisponibles,
-                "docksAvailable": element.fields.nombreemplacementsdisponibles,
-                "createdAt": element.fields.lastupdate
-            })
-
-        })
-    })
-        .catch(error => {
-            console.log(error);
-        });
-}
-
-function GetDataVille(){
-    //on va charger les informations de nous 4 villes ici;
-    getDLille();
-    getDParis();
-    //getDLyon();
-    getDRennes()
-}
-
+/* ----- FUNCTIONS ----- */
 
 function getDLille(){
     axiosLille.then( response => {
@@ -288,8 +168,130 @@ function getDRennes(){
 }
 
 
+function setDLille(){
+    axiosLille.then(reponse => {
+        reponse.data.records.forEach(function (element){
+            var size = element.fields.nbvelosdispo + element.fields.nbplacesdispo;
+            //on charge la collection stations_dynamic_history
+            stations_static
+                .updateOne(
+                    {"stationId": element.recordid}, // Filter
+                    {$set: {
+
+                            "city": element.fields.commune.toUpperCase(),
+                            "name": element.fields.nom.toUpperCase(),
+                            "geolocation": element.fields.localisation,
+                            "size": size,
+                            "tpe": element.fields.type.includes("AVEC TPE"),
+                            "available": null,
+                            "updatedAt": element.fields.datemiseajour
+                        }} // Update
+                )
+                .then((obj) => {
+                    console.log('Updated - ' + obj);
+                    response.redirect('orders')
+                })
+                .catch((err) => {
+                    console.log('Error: ' + err);
+                })
+            stations_dynamic.insertOne({
+                "stationId": element.recordid,
+                "bikesAvailable": element.fields.nbvelosdispo,
+                "docksAvailable": element.fields.nbplacesdispo,
+                "createdAt": element.fields.datemiseajour
+            })
+        })
+
+        //on charge la collection stations_dynamic
 
 
+    })
+
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+function setDParis(){
+    axiosParis.then(reponse => {
+        reponse.data.records.forEach(function (element){
+            //on charge la collection stations_dynamic_history
+            stations_static
+                .updateOne(
+                    {"stationId": element.recordid}, // Filter
+                    {$set: {
+                            "city": element.fields.nom_arrondissement_communes.toUpperCase(),
+                            "name": element.fields.name.toUpperCase(),
+                            "geolocation": element.fields.coordonnees_geo,
+                            "size": element.fields.capacity,
+                            "tpe": element.fields.is_renting.includes("OUI"),
+                            "available": element.fields.is_installed.includes("OUI"),
+                            "updatedAt": element.record_timestamp
+                        }} // Update
+                )
+                .then((obj) => {
+                    console.log('Updated - ' + obj);
+                    response.redirect('orders')
+                })
+                .catch((err) => {
+                    console.log('Error: ' + err);
+                })
 
 
+            //on charge la collection stations_dynamic
+            var bikesAvailable = element.fields.capacity - element.fields.numdocksavailable;
+            stations_dynamic.insertOne({
+                "stationId": element.recordid,
+                "bikesAvailable": bikesAvailable,
+                "docksAvailable": element.fields.numdocksavailable,
+                "createdAt": element.record_timestamp
+            })
 
+        })
+    })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+function setDRennes(){
+    var city = "RENNES";
+    var tpe = false;
+    axiosRennes.then(reponse => {
+        reponse.data.records.forEach(function (element){
+            //on charge la collection stations_dynamic_history
+            stations_static
+                .updateOne(
+                    {"stationId": element.recordid}, // Filter
+                    {$set: {
+                            "city": city.toUpperCase(),
+                            "name": element.fields.nom.toUpperCase(),
+                            "geolocation": element.fields.coordonnees,
+                            "size": element.fields.nombreemplacementsactuels,
+                            "tpe": tpe,
+                            "available": null,
+                            "updatedAt": element.fields.lastupdate
+                        }} // Update
+                )
+                .then((obj) => {
+                    console.log('Updated - ' + obj);
+                    response.redirect('orders')
+                })
+                .catch((err) => {
+                    console.log('Error: ' + err);
+                })
+
+            //on charge la collection stations_dynamic
+            stations_dynamic.insertOne({
+                "stationId": element.recordid,
+                "bikesAvailable": element.fields.nombrevelosdisponibles,
+                "docksAvailable": element.fields.nombreemplacementsdisponibles,
+                "createdAt": element.fields.lastupdate
+            })
+
+        })
+    })
+        .catch(error => {
+            console.log(error);
+        });
+}
