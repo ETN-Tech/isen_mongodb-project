de8d5cf6e217883a88531b0cc1363b95febd885f
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-10-04 16:42:46 +0200

    Added deactivate stations functionnality

--- react-ui/src/app/business/Business.js
+++ react-ui/src/app/business/Business.js
@@ -3,6 +3,11 @@ import axios from "axios";
 
 function Business() {
     const [ stationResults, setStationResults ] = useState([]);
+    
+    const [ x1, setX1 ] = useState(0);
+    const [ x2, setX2 ] = useState(0);
+    const [ y1, setY1 ] = useState(0);
+    const [ y2, setY2 ] = useState(0);
 
     function findStationByName(searchText) {
         axios.get( process.env.REACT_APP_API_HOST+ "/api/stations/find/" + encodeURIComponent(searchText))
@@ -13,9 +18,33 @@ function Business() {
             .catch(err => console.log(err));
     }
 
+    function deactivateStations() {
+        axios.get(process.env.REACT_APP_API_HOST+ "/api/stations/deactivate/"+ x1 +"/"+ y1 +"/"+ x2 +"/"+ y2)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+    }
+
     return (
         <>
             <h1>Business App</h1>
+
+            <h2>Deactivate stations in zone</h2>
+            
+            <label htmlFor="x1">X1</label><br/>
+            <input type="text" id="x1" onChange={e => setX1(e.target.value)}/>
+            <br/>
+            <label htmlFor="x2">X2</label><br/>
+            <input type="text" id="x2" onChange={e => setX2(e.target.value)}/>
+            <br/>
+            <label htmlFor="y1">Y1</label><br/>
+            <input type="text" id="y1" onChange={e => setY1(e.target.value)}/>
+            <br/>
+            <label htmlFor="y2">Y2</label><br/>
+            <input type="text" id="y2" onChange={e => setY2(e.target.value)}/>
+            <br/>
+            <button onClick={deactivateStations}>Update</button>
+
+            <hr/>
 
             <h2>Find station by name</h2>
             <input type="text" id="search" placeholder='Type something' onChange={e => findStationByName(e.target.value)} />
